f2b734cc7ec83d55e069b29c1bf0121c16e20eea
Alberto Matos <alberto@pmibrasil.eti.br>
2020-08-20 22:33:57 +0000

    refactor: simplify code on verify required fields

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,12 +2,12 @@ import { badRequest } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 export class SignUpController {
-  handle (httpResponse: HttpRequest): HttpResponse {
-    if (!httpResponse.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpResponse.body.email) {
-      return badRequest(new MissingParamError('email'))
+  handle (httpResquest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpResquest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }

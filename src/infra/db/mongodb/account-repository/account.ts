228d24b52a3f60186da9d9303f2f9f21b4d98441
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-09 22:42:38 +0000

    feat: ensure reconnect if mongodb is client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,7 +2,7 @@ import { AddAccountRepository, AddAccountModel, AccountModel } from './account-p
 import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
     return MongoHelper.map(account)
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,7 +1,10 @@
 import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
+  uri: null as string,
   client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -9,10 +12,16 @@ export const MongoHelper = {
   },
 
   async disconnect (): Promise<void> {
-    this.client.close()
+    if (this.client) {
+      this.client.close()
+      this.client = null
+    }
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
 

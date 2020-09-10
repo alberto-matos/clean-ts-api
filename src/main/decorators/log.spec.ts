94b4ddaf0124130dcab5af42fe1865dee5ecd467
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-10 00:17:24 +0000

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,24 +1,41 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/controllers/signup/signup-protocols'
 import { LogControllerDecorator } from './log'
 
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'any_name',
+          email: 'any_email@email.com',
+          password: 'any_password'
+        }
+      }
+      return await new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+interface sutType {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): sutType => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('Log Controller Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'any_name',
-            email: 'any_email@email.com',
-            password: 'any_password'
-          }
-        }
-        return await new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStup = new ControllerStub()
-    const sut = new LogControllerDecorator(controllerStup)
-    const handleStub = jest.spyOn(controllerStup, 'handle')
+    const { sut, controllerStub } = makeSut()
+    const handleStub = jest.spyOn(controllerStub, 'handle')
     const httpRequest: HttpRequest = {
       body: {
         name: 'Daiane',

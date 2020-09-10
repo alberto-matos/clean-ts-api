17a0187acd2bc2b8260694ed9f4ad2f6eefb22e2
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-10 00:02:16 +0000

    feat: ensure LogControllerDecorator calls controller handle

---
+++ src/main/fatories/index.ts
@@ -0,0 +1,7 @@
+export * from '../../presentation/controllers/signup/signup'
+export * from '../../utils/email-validator-adapter'
+export * from '../../data/usercases/add-account/db-add-account'
+export * from '../../infra/criptography/bcrypt-adapter'
+export * from '../../infra/db/mongodb/account-repository/account'
+export * from '../../presentation/controllers/signup/signup-protocols'
+export * from '../decorators/log'

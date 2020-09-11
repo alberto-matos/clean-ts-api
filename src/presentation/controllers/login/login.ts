ab5724909a36fc32bad018523ee073321bbc5b91
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-11 18:11:47 +0000

    feat: ensure call Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -3,26 +3,32 @@ import { Controller } from '../../protocols/controller'
 import { badRequest, serverError } from '../../../presentation/helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
+import { Authentication } from '../../../domain/usecases/authentication'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly authentication: Authentication
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator
+    this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requeredFields = ['email', 'password']
+      const { email, password } = httpRequest.body
       for (const field of requeredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { email } = httpRequest.body
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      await this.authentication.auth(email, password)
+
       return null
     } catch (error) {
       return serverError(error)

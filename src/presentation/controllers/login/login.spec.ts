c3a9039064c5559866b9b66a0f00017ab06f80db
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-11 13:32:38 +0000

    refactor: move creation sut and httpRequest to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,17 +1,32 @@
 import { badRequest } from '../../../presentation/helpers/http-helper'
+import { HttpRequest } from '../../protocols'
 import { MissingParamError } from '../../errors'
 import { LoginController } from './login'
 
+const makeRequest = (): HttpRequest => {
+  return {
+    body: {
+      email: 'any_email@email.com',
+      password: 'any_password'
+    }
+  }
+}
+const makeSut = (): LoginController => {
+  return new LoginController()
+}
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provider', async () => {
-    const httpRequest = { body: { password: 'any_password' } }
-    const sut = new LoginController()
+    const httpRequest = makeRequest()
+    delete (httpRequest.body.email)
+    const sut = makeSut()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('Should return 400 if no password is provider', async () => {
-    const httpRequest = { body: { email: 'any_mail' } }
-    const sut = new LoginController()
+    const httpRequest = makeRequest()
+    delete (httpRequest.body.password)
+    const sut = makeSut()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })

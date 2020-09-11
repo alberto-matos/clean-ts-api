ed62d7b398d1da011c3034ba37dfef7e7fe5708f
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-11 13:40:39 +0000

    refactor: alter return of makeSut

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,10 @@ import { HttpRequest } from '../../protocols'
 import { MissingParamError } from '../../errors'
 import { LoginController } from './login'
 
+interface sutTypes {
+  sut: LoginController
+}
+
 const makeRequest = (): HttpRequest => {
   return {
     body: {
@@ -11,22 +15,24 @@ const makeRequest = (): HttpRequest => {
     }
   }
 }
-const makeSut = (): LoginController => {
-  return new LoginController()
+const makeSut = (): sutTypes => {
+  return {
+    sut: new LoginController()
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provider', async () => {
     const httpRequest = makeRequest()
     delete (httpRequest.body.email)
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('Should return 400 if no password is provider', async () => {
     const httpRequest = makeRequest()
     delete (httpRequest.body.password)
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })

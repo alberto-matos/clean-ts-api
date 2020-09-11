14fd9d93106c83393b6b5baade593becd70895c5
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-11 14:51:48 +0000

    feat: ensure call EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,8 +2,15 @@ import { HttpRequest, HttpResponse } from '../../protocols'
 import { Controller } from '../../protocols/controller'
 import { badRequest } from '../../../presentation/helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../../protocols/email-validator'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requeredFields = ['email', 'password']
     for (const field of requeredFields) {
@@ -11,6 +18,8 @@ export class LoginController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    const { email } = httpRequest.body
+    this.emailValidator.isValid(email)
     return null
   }
 }

bd2481a80f301466f25ffaecfbf1128eef9460aa
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-16 18:40:46 +0000

    test: ensure CompareFieldsValidation returns InvalidParamerror if validation fails

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+const makeFakeInput = (): Object => {
+  return { field: 'any_content', fieldToCompare: 'any_content' }
+}
+
+describe('Required Field Validation', () => {
+  test('Should return a InvalidParamError if RequiredFieldValidation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate(Object.assign({}, { ...makeFakeInput() }, { fieldToCompare: 'other_content' }))
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+})

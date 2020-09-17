44b48978944879f7bc49698d7ca99e98d11d9146
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-17 13:40:45 +0000

    refactor: group validation imports in a single file

--- src/main/fatories/signup/signup-validation.ts
+++ src/main/fatories/signup/signup-validation.ts
@@ -1,9 +1,6 @@
 import { Validation } from '../../../presentation/protocols/validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation, CompareFieldsValidation } from '../../../presentation/helpers/validators'
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations: Validation[] = []

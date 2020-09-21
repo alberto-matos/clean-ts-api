6cea1d929bbbd0645bb22590d9f701ce4ab63e23
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-21 13:39:30 +0000

    refactor: move imports to a single file

--- src/data/usercases/authentication/db-authentication.spec.ts
+++ src/data/usercases/authentication/db-authentication.spec.ts
@@ -1,10 +1,4 @@
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { AccountModel } from '../../../domain/models/account'
-import { DbAuthentication } from './db-authentication'
-import { AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import { AuthenticationModel, LoadAccountByEmailRepository, HashComparer, TokenGenerator, UpdateAccessTokenRepository, AccountModel, DbAuthentication } from './db-authentication-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',

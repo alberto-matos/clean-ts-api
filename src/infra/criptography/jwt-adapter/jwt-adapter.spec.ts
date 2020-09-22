5e773745fe6ecf08d69b78f1617d38ad564d3aaf
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-22 13:13:04 +0000

    test: ensure JwtAdapter returns a AccessToken on sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,6 +2,12 @@ import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 import { JwtAdapter } from './jwt-adapter'
 import jwt from 'jsonwebtoken'
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'any_token'
+  }
+}))
+
 const makeSut = (): Encrypter => {
   return new JwtAdapter('secretKey')
 }
@@ -12,5 +18,10 @@ describe('Jwt Adapter', () => {
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secretKey')
+  })
+  test('Should return an AccessToken on sign succeed', async () => {
+    const sut = makeSut()
+    const token = await sut.encrypt('any_value')
+    expect(token).toBe('any_token')
   })
 })

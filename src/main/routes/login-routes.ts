a96bd15396110ccaf24b4dad7db521c683702863
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-22 20:46:22 +0000

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
-import { makeSignupController } from '../fatories/signup/signup-factory'
 import { adaptRoute } from '../config/adapters/express/express-route-adapter'
+import { makeSignupController } from '../fatories/signup/signup-factory'
+import { makeLoginController } from '../fatories/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }

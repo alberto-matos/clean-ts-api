40cd7cce9532fd93e71916141d3622ee4edaf2f6
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-23 17:39:42 +0000

    refactor: changed message Server running at 192.168.0.5:<port> to Server running on port: <port>

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import env from '../main/config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Server running at http://192.168.0.5:${env.port}`))
+    app.listen(env.port, () => console.log(`Server running on port: ${env.port}`))
   })
   .catch(console.error)

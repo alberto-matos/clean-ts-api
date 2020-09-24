46af0193f42eef7abdb27b736be02780b95288ca
Alberto Matos <alberto@pmibrasil.eti.br>
2020-09-24 21:45:29 +0000

    refactor: move creation Authentication to the helper factor method

--- src/main/fatories/signup/signup-factory.ts
+++ src/main/fatories/signup/signup-factory.ts
@@ -1,12 +1,14 @@
 import { SignUpController, DbAddAccount, BcryptAdapter, AccountMongoRepository, Controller, LogControllerDecorator, LogMongoRepository } from '.'
 import env from '../../config/env'
+import { makeAuthentication } from '../authentication/authentication-factory'
 import { makeSignupValidation } from './signup-validation-factory'
 
 export const makeSignupController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(env.salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signupController = new SignUpController(dbAddAccount, makeSignupValidation())
+  const authentication = makeAuthentication()
+  const signupController = new SignUpController(dbAddAccount, makeSignupValidation(), authentication)
   const logMongoReposiory = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logMongoReposiory)
 }

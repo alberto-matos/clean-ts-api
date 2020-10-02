e74cdc6dfb00af4faa7520d09ca703d83f39b5b8
Alberto Matos <alberto@pmibrasil.eti.br>
2020-10-02 13:16:09 +0000

    feat: ensure DbLoadAccountByToken returns null Dencrypter returns null

--- src/data/usercases/load-account-by-token/load-account-by-token.ts
+++ src/data/usercases/load-account-by-token/load-account-by-token.ts
@@ -6,7 +6,15 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly decrypter: Decrypter) { }
 
   async load (accessToken: string, role?: string): Promise<AccountModel> {
-    await this.decrypter.decrypt(accessToken)
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      return {
+        id: 'any',
+        name: 'any',
+        email: 'any',
+        password: 'any'
+      }
+    }
     return null
   }
 }

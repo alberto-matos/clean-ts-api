b48f4327de32ff53e1c4fc5c454ef643b79494f6
alberto-matos <alberto@pmibrasil.eti.br>
2020-10-19 20:01:30 +0000

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,17 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 import { AddSurveyRepository, AddSurveyModel, MongoHelper, Collection } from './survey-mongo-repository-protocols'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   private async getAccountCollection (): Promise<Collection> {
     return await MongoHelper.getCollection('surveys')
   }
 
   async add (surveyData: AddSurveyModel): Promise<void> {
     await (await this.getAccountCollection()).insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    return await (await this.getAccountCollection()).find().toArray()
   }
 }

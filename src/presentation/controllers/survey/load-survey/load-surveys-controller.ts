9979f3d297b938d1d60bd6a9e5d279d451d5e89e
alberto-matos <alberto@pmibrasil.eti.br>
2020-10-19 15:13:41 +0000

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
@@ -1,11 +1,11 @@
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
-import { Controller, HttpRequest, HttpResponse } from './load-surveys-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, ok } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurvey: LoadSurveys) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurvey.load()
-    return null
+    const surveys = await this.loadSurvey.load()
+    return ok(surveys)
   }
 }

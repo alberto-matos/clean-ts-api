ac6904dc1e6f748cacc70a772f52c8a462d7662d
alberto-matos <alberto@pmibrasil.eti.br>
2020-10-19 18:20:53 +0000

    test: ensure DbLoadSurveys returns a list of Surveys on success

--- src/data/usercases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usercases/load-surveys/db-load-surveys.spec.ts
@@ -52,4 +52,10 @@ describe('DbLoadSurveys', () => {
     await sut.load()
     expect(loadSpy).toBeCalled()
   })
+
+  test('Should return a list of Surveys on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.load()
+    expect(httpResponse).toEqual(makeFakeSurveys())
+  })
 })

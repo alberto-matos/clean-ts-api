b67634ad7f4d08ddf34908c8542272e969db22c4
alberto-matos <alberto@pmibrasil.eti.br>
2020-10-19 17:02:20 +0000

    feat: ensure LoadSurveysController returns 204 if LoadSurveys returns empty

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
@@ -1,4 +1,5 @@
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
+import { noContent } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, ok, serverError } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
@@ -7,7 +8,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurvey.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }

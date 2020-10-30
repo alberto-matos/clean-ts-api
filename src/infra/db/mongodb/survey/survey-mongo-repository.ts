5ab361858ed2a96bee2f0365300a7a2f7511dad5
Alberto Matos <alberto@pmibrasil.eti.br>
2020-10-30 20:27:46 +0000

    feat: ensure LoadById returns correct survey on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,8 +1,9 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-survey-repository'
 import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyRepository, AddSurveyModel, MongoHelper, Collection } from './survey-mongo-repository-protocols'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   private async getAccountCollection (): Promise<Collection> {
     return await MongoHelper.getCollection('surveys')
   }
@@ -13,5 +14,10 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
 
   async loadAll (): Promise<SurveyModel[]> {
     return await (await this.getAccountCollection()).find().toArray()
+  }
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const survey = await (await this.getAccountCollection()).findOne({ _id: id })
+    return survey
   }
 }

106187b947ad6c28d19404fc243c49c46b744bcc
Alberto Matos <alberto@pmibrasil.eti.br>
2020-11-03 15:35:06 +0000

    feat: ensure Save() add a survey result if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,24 @@
+import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultModel, MongoHelper } from './survey-result-mongo-repository-protocols'
+import { Collection } from 'mongodb'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  private async getAccountCollection (): Promise<Collection> {
+    return await MongoHelper.getCollection('surveyResults')
+  }
+
+  async save (surveyData: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResult = await (await this.getAccountCollection()).findOneAndUpdate({
+      surveyId: surveyData.surveyId,
+      accountId: surveyData.accountId
+    }, {
+      $set: {
+        answer: surveyData.answer,
+        date: surveyData.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    return MongoHelper.map(surveyResult.value)
+  }
+}

74ccbe83a40b405ce973312174637a1b68f80854
Alberto Matos <alberto@pmibrasil.eti.br>
2020-11-03 19:38:51 +0000

    feat: ensure SaveSurveyResult call LoadSurveyById with correct value

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '@/presentation/protocols'
+export * from '@/domain/usecases/survey/load-surveys-by-id'
+export * from '../../survey/load-survey/load-surveys-controller-protocols'
+export * from '@/domain/usecases/survey/load-surveys-by-id'

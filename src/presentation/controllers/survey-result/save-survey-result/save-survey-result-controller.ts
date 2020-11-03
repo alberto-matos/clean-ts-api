acb37e2ca4b9686a4bb9854c4642a9163d1fa122
Alberto Matos <alberto@pmibrasil.eti.br>
2020-11-03 21:37:56 +0000

    feat: ensure SaveSurveyResult returns 403 if answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -6,6 +6,11 @@ export class SaveSurveyResultController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const survey = await this.loadSurveyById.loadById(httpRequest.params?.surveyId)
+      const { answer } = httpRequest.body
+      if (survey) {
+        const answers = survey.answers.map(item => item.answer)
+        return answers.includes(answer) ? ok(survey) : forbidden(new InvalidParamError('answer'))
+      }
       return survey ? ok(survey) : forbidden(new InvalidParamError('surveyId'))
     } catch (error) {
       return serverError(error)

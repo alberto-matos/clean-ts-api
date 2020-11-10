44112f6861615ed23842b1103ee0784d06dcc995
alberto-matos <alberto@pmibrasil.eti.br>
2020-11-10 15:18:44 -0300

    refactor: use mockLoadSurveysRepository and mockSurveysModel

--- src/data/test/mock-db-survey.ts
+++ src/data/test/mock-db-survey.ts
@@ -1,8 +1,8 @@
-import { mockSurveyModel } from '@/domain/test'
+import { mockSurveyModel, mockSurveysModel } from '@/domain/test'
 import { AddSurveyRepository } from '../protocols/db/survey/add-survey-repository'
 import { LoadSurveyByIdRepository } from '../protocols/db/survey/load-survey-by-id-repository'
 import { AddSurveyParams } from '../usercases/survey/add-survey/db-add-survey-protocols'
-import { SurveyModel } from '../usercases/survey/load-surveys/db-load-surveys-protocols'
+import { LoadSurveysRepository, SurveyModel } from '../usercases/survey/load-surveys/db-load-surveys-protocols'
 
 export const mockAddSurveyRepository = (): AddSurveyRepository => {
   class AddSurveyRepositoryStub implements AddSurveyRepository {
@@ -20,4 +20,13 @@ export const mockLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
     }
   }
   return new LoadSurveyByIdRepositoryStub()
+}
+
+export const mockLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async loadAll (): Promise<SurveyModel[]> {
+      return await new Promise(resolve => resolve(mockSurveysModel()))
+    }
+  }
+  return new LoadSurveysRepositoryStub()
 }

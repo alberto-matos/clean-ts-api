1a98e5ac17a394d0cdb920a3fc67ec44bf7015dd
alberto-matos <alberto@pmibrasil.eti.br>
2020-11-10 15:47:27 -0300

    refactor: change simplifed mockAccountModel

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -2,18 +2,13 @@ import { AccountModel } from '@/domain/models/account'
 import { AddAccountParams } from '@/domain/usecases/account/add-account'
 import { AuthenticationParams } from '../usecases/account/authentication'
 
-export const mockAccountModel = (): AccountModel => ({
-  id: 'any_id',
-  name: 'any_name',
-  email: 'any_email@email.com',
-  password: 'hashed_password'
-})
-
 export const mockAddAccountParams = (): AddAccountParams => ({
   name: 'any_name',
   email: 'any_email@email.com',
   password: 'any_password'
 })
+
+export const mockAccountModel = (): AccountModel => (Object.assign({}, mockAddAccountParams(), { id: 'any_id' }, { password: 'hashed_password' }))
 
 export const mockAuthenticationParams = (): AuthenticationParams => ({
   email: 'any_email@email.com',

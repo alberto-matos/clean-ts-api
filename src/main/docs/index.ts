dafa6e33656adc81b145c28f605c975bd6c49700
alberto-matos <alberto@pmibrasil.eti.br>
2020-11-11 10:10:44 -0300

    feat: add swagger-api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,13 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean API - by Alberto Matos',
+    description: `Projeto destinado ao aprendizado de: \n
+      * nodeJs \n
+      * TypeScript \n
+      * Clean architecture \n
+      * SOLID \n
+      * TDD`,
+    version: '1.0.0'
+  }
+}

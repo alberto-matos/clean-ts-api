2103dbcac7d550ea2146da3e25c1cc6a0a3aa0b5
alberto-matos <alberto@pmibrasil.eti.br>
2020-11-18 17:51:42 -0300

    feat: ensure LoadSyrveyResyult calls LoadSurveyResult with correct value

--- src/presentation/controllers/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/load-survey-result/load-survey-result-controller.ts
@@ -1,18 +1,22 @@
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { Controller, forbidden, HttpRequest, HttpResponse, InvalidParamError, serverError } from '../login/login/login-controller-protocols'
 import { LoadSurveyById } from '../survey-result/save-survey-result/save-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) { }
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveyId = httpRequest.params?.surveyId
-      if (surveyId) {
-        const survey = await this.loadSurveyById.loadById(surveyId)
-        if (!survey) {
-          return forbidden(new InvalidParamError('SurveyId'))
-        }
+      const survey = await this.loadSurveyById.loadById(surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('SurveyId'))
       }
+      await this.loadSurveyResult.load(surveyId)
+
       return null
     } catch (error) {
       return serverError(error)
--- src/presentation/test/mock-load-survey-result.ts
+++ src/presentation/test/mock-load-survey-result.ts
@@ -1,5 +1,6 @@
-import { mockSurveyModel } from '@/domain/test'
-import { HttpRequest, LoadSurveyById, SurveyModel } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols'
+import { mockSurveyModel, mockSurveyResultModel } from '@/domain/test'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+import { HttpRequest, LoadSurveyById, SurveyModel, SurveyResultModel } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols'
 
 export const mockLoadSurveyById = (): LoadSurveyById => {
   class LoadSurveyByIdStub implements LoadSurveyById {
@@ -16,4 +17,13 @@ export const mockHttpRequest = (): HttpRequest => {
       surveyId: 'any_id'
     }
   }
+}
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultStub()
 }
